8adbf09692c0867618e34123616a860f1b7ae27e
lee Kyung young <leekyungyoung@leeui-MacBook-Pro-2.local>
2018-12-28 18:35:18 +0900

    2018-12-28 Add WebPage

--- Project/routes/index.js
+++ Project/routes/index.js
@@ -4,11 +4,18 @@ const QUZCoinRouter = require('./QUZCoinRouter');
 const QUZWalletRouter = require('./QUZWalletRouter');
 const commonRouter = require('./commonRouter');
 const todoRouter = require('./todoRouter');
+const path = require('path');
 
 const router = (app) => {
+
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/views/index.html'));
+});
+app.get('/sponsor',function(req,res){
+  res.sendFile(path.join(__dirname+'/views/sponsor.html'));
+});
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
-
   app.use('/quzlive-quiz', QUZLiveQuizRouter);
   app.use('/api/quzcoin', QUZCoinRouter);
   app.use('/api/wallet', QUZWalletRouter);
